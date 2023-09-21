efc5bd09bdc74988d44050fbebd2b2b6c793fe97
riswan29 <wandra290401@gmail.com>
2023-09-21 08:36:13 +0700

    selesai membuat model user , register ke admin dan buat md untuk akun superadmin

---
+++ acountAdmin.md
@@ -0,0 +1,3 @@
+1. email : admin@gmail.com | username : herlyn | password : root
+
+2. email : daffa@gmail.com | username : daffa | password : root
--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    image = models.ImageField(upload_to='img_profile/')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
