e61b7d3f76a27fd279fd2cfaf430d65ad1364e77
riswan29 <wandra290401@gmail.com>
2023-10-20 08:56:39 +0700

    add template user login and dash to backend

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,23 @@
 from .models import *
 from django import forms
-
+#  address = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control resume', 'placeholder': 'Jl. xxxxx', 'rows' :'5'}),
 class RegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(attrs={'type':'text', 'placeholder':'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'type':'email', 'placeholder':'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'type':'password','placeholder':'Password'}))
+    # image = forms.FileField(widget=forms.FileInput(attrs={'type':'file','class':'form-controll'}))
+    image = forms.FileField(widget=forms.FileInput(attrs={'type':'file','class':'form-controll'}))
 
     class Meta:
         model = customUser
         fields = ('username', 'email', 'password', 'image',)
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'type':'password', 'placeholder': 'Password'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'type':'email', 'placeholder':'Email'}))
+    class Meta:
+        email = forms.EmailField()
+        password = forms.CharField(widget=forms.PasswordInput)
 
 class ProductForm(forms.ModelForm):
     class Meta:
